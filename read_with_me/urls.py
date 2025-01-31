496295bf47ab58cedca2fe0fec165b827e2308a9
Ayisha Sandiford <ish.equal@gmail.com>
2025-01-30 21:48:38 -0400

    Setup database access and hid secrect key

--- read_with_me/urls.py
+++ read_with_me/urls.py
@@ -20,4 +20,5 @@ from home import views as index_views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', index_views.index, name='index'),
+    path('accounts/', include('allauth.urls')),
 ]
